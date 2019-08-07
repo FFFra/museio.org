514ebccd1a67af1068c5b43f985ae689506e0127
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-08-07 20:43:54 +0100

    setup react-router-dom

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import GalleriesDetails from './pages/GalleriesDetails';
 import Stories from './pages/Stories';
 import StoriesDetails from './pages/StoriesDetails';
 import Error from './pages/Error';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home />
-      <About />
-      <Galleries />
-      <GalleriesDetails />
-      <Stories />
-      <StoriesDetails />
-      <Error />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about/" component={About} />
+        <Route exact path="/galleries/" component={Galleries} />
+        <Route exact path="/galleries/:slug" component={GalleriesDetails} />
+        <Route exact path="/stories" component={Stories} />
+        <Route exact path="/stories/:slug" component={StoriesDetails} />
+        <Route component={Error} />
+      </Switch>
     </div>
   );
 }
