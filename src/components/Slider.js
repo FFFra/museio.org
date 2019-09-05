0954f4a1f26b5a74ea3963100315d98025afe872
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-09-05 16:35:16 +0100

    remove tiny slider. Add react-slick

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
-import { ArtContext } from '../context';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default class Slider extends Component {
-
-  static contextType = ArtContext;
-
+export default class MultipleItems extends Component {
   render() {
-    // const { name, age } = this.context;
-
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 3,
+      slidesToScroll: 3
+    };
     return (
       <div>
-        I'm  and I'm years old
+        <h2> Multiple items </h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div>
+          <div>
+            <h3>9</h3>
+          </div>
+        </Slider>
       </div>
-    )
+    );
   }
 }
