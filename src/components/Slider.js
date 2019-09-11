9aa5752ee491d42d93a3170d3f85b5b9453e2bce
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-09-11 18:08:56 +0100

    Home slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,50 +1,35 @@
 import React, { Component } from 'react';
+import { ArtContext } from '../context';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default class MultipleItems extends Component {
+  static contextType = ArtContext;
+
   render() {
     const settings = {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 3,
+      slidesToShow: 5,
       slidesToScroll: 3
     };
+
+    let { featured: stories } = this.context;
+    stories = stories.map(storie => {
+      return (
+        <div key={storie.id}>
+          <img src={storie.photo} alt="" />
+          <h1>{storie.title}</h1>
+        </div>
+      )
+    });
+
     return (
-      <div>
-        <h2> Multiple items </h2>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-          <div>
-            <h3>9</h3>
-          </div>
-        </Slider>
-      </div>
+      <Slider {...this.settings}>
+        {stories}
+      </Slider>
     );
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,13 @@ import Slider from '../components/Slider';
 
 const Home = () => {
   return (
-    <div>
-      Hello from home
+    <>
       <Slider />
-    </div>
+      <h1>Museio is a project to collect audio and video stories about art</h1>
+      <h3>Next time you're in an art gallery, take Museio with you to discover the stories behind your favourite artworks, narrated by experts and fans around the world</h3>
+      <h2>Featured Destinations</h2>
+      <h2>Featured Stories</h2>
+    </>
   )
 }
 
