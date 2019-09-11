fcf64ddbb8e9a175f6a2d2989075a104336bf736
Flávio Fraschetti <flavio.fraschetti@indigoway.com>
2019-09-11 20:46:18 +0100

    checkpoint before refactor home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Slider from '../components/Slider';
+import GalleryCard from '../components/GalleryCard';
 
 
 const Home = () => {
   return (
-    <>
+    <section>
       <Slider />
       <h1>Museio is a project to collect audio and video stories about art</h1>
       <h3>Next time you're in an art gallery, take Museio with you to discover the stories behind your favourite artworks, narrated by experts and fans around the world</h3>
       <h2>Featured Destinations</h2>
+      <GalleryCard />
       <h2>Featured Stories</h2>
-    </>
+    </section>
   )
 }
 
